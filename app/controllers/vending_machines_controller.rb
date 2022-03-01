c9c3fda243ce00924bb583b6fa7dcf5b7375849c
amine achouri <ahmedamine.achouri@gmail.com>
2022-03-01 17:58:03 +0100

    Vending machine new view, Login can now attributed user type

--- app/controllers/vending_machines_controller.rb
+++ app/controllers/vending_machines_controller.rb
@@ -1,54 +1,53 @@
 class VendingMachinesController < ApplicationController
+  def index
+    @vending_machines = VendingMachine.all
 
-    def index
-        @vending_machines = VendingMachine.all
-
-        @markers = @vending_machines.geocoded.map do |vending_machine|
-            {
-              lat: venue.latitude,
-              lng: venue.longitude,
-              info_window: render_to_string(partial: "info_window", locals: { vending_machine: vending_machine })
-            }
+    @markers = @vending_machines.geocoded.map do |vending_machine|
+      {
+        lat: venue.latitude,
+        lng: venue.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { vending_machine: vending_machine })
+      }
     end
+  end
 
-    def show
-        @vending_machine = VendingMachine.find(params[:id])
-    end
+  def show
+    @vending_machine = VendingMachine.find(params[:id])
+  end
 
-    def new
-        @vending_machine = VendingMachine.new
-    end
+  def new
+    @vending_machine = VendingMachine.new
+  end
 
-    def create
-        @vending_machine = VendingMachine.new(vending_machine_params)
-        @vending_machine.user = current_user
-        if @vending_machine.save
-          redirect_to vending_machine_path(@vending_machine)
-        else
-          render :new
-        end
+  def create
+    @vending_machine = VendingMachine.new(vending_machine_params)
+    @vending_machine.user = current_user
+    if @vending_machine.save
+      redirect_to vending_machine_path(@vending_machine)
+    else
+      render :new
     end
+  end
 
-    def edit
-        @vending_machine = VendingMachine.find(params[:id])
-    end
+  def edit
+    @vending_machine = VendingMachine.find(params[:id])
+  end
 
-    def update
-        @vending_machine = VendingMachine.find(params[:id])
-        @vending_machine.update(vending_machine_params)
-        redirect_to vending_machine_path(@vending_machine)
-    end
+  def update
+    @vending_machine = VendingMachine.find(params[:id])
+    @vending_machine.update(vending_machine_params)
+    redirect_to vending_machine_path(@vending_machine)
+  end
 
-    def destroy
-        @vending_machine = VendingMachine.find(params[:id])
-        @vending_machine.destroy
-        redirect_to vending_machines_path
-    end
+  def destroy
+    @vending_machine = VendingMachine.find(params[:id])
+    @vending_machine.destroy
+    redirect_to vending_machines_path
+  end
 
-    private
+  private
 
-    def venue_params
-        params.require(:vending_machine).permit(:address, :description)
-    end
-    end
+  def venue_params
+    params.require(:vending_machine).permit(:address, :description)
+  end
 end
