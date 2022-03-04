6c49d4fb1dad1fe0f59735250cf169a16a9707fd
amine achouri <ahmedamine.achouri@gmail.com>
2022-03-04 12:02:56 +0100

    Test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,42 +5,46 @@ class PagesController < ApplicationController
   end
 
   def dashboard_vendor
+    # Vending Machines
     @vending_machines = VendingMachine.where(user: current_user)
     @vm_number = @vending_machines.count
+    # Products
     @products = Product.all
+    # Booking management
+    @my_bookings = Bookings.vending_machine_id.user_id.where(user: current_user)
+    @booking = Booking.new
   end
-  
+
   def dashboard
 
-    # @vendor_vending_machines = VendingMachine.where(user_id: current_user) 
+    # @vendor_vending_machines = VendingMachine.where(user_id: current_user)
     # @my_bookings = Booking.where(vending_machine_id: vending_machine_id)
     # @my_bookings = Booking.all.select do |booking|
     #   booking.user_id == current_user.id
-    # end 
+    # end
 
     # VendingMachine.all.each do |machine|
     #   @total_quantity = 0
     #   machine.Item.all.each do |item|
     #     @total_quantity += item.quantity
-    #   end 
-    # end 
+    #   end
+    # end
 
     # @total_capacity = 0
     # Item.all.each do |item|
     #   @total_capacity += item.capacity
-    # end 
+    # end
 
     @vending_machine = VendingMachine.all
     @total_sum = @vending_machine.map do |machine|
       items = Item.where(vending_machine_id: machine.id)
-        quantity_items = items.map do |item| 
+        quantity_items = items.map do |item|
           item.quantity
-        end 
+        end
       quantity_items.sum
-    end 
-    # total_quantity += item.quantity 
+    end
+    # total_quantity += item.quantity
 
-    @vending_machines = VendingMachine.where(@total_sum < 150) 
+    @vending_machines = VendingMachine.where(@total_sum < 150)
   end
-end 
-
+end
