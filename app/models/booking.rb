33c1cb4a89358836925b79eb1bb1c53477b8d345
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-03-10 13:03:39 +0100

    refiller & vendor dashboards make the ongoing refill RAG according to the expiry date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :vending_machine
   belongs_to :user
+
+
+  def color
+    if (created_at + 90.minutes) > Time.now
+      "#1edd88" # RAG - Green
+    elsif (created_at + 120.minutes) > Time.now
+      "#FFC65A" # RAG - Amber
+    else
+      "#dd6860" # RAG - Red
+    end
+  end
+
+  #  "#FFC65A" !!! or #E67E22 # RAG - Amber
 end
